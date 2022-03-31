8488067ab1661d4fe70b38ba9c6ff0cdbb052850
BMhome\Roger.Feng <roger.feng@baimeihome.com>
2022-03-31 15:45:01 +0800

    fyb/20220331/test2

---
+++ src/components/tab/index.js
@@ -0,0 +1,3 @@
+import tab from "@/components/tab/tab.vue";
+import './index.css'
+export default tab
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,34 @@
+
+
+<script>
+export default {
+  name: "myTab",
+  data () {
+    return {
+      fullPathList: [],
+    }
+  },
+  created () {
+    console.log(11111111111111111)
+    console.log('fyb/20220331/test')
+    console.log('fyb/20220331/test')
+  },
+  methods: {
+
+  },
+  watch: {
+
+  },
+  render () {
+    return (
+        <div class="ant-pro-multi-tab">
+          <p>你好</p>
+        </div>
+    )
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
